e8e3a95882f33b438f87693538608ae9c9392868
Joshua Frías <metatron1495@gmail.com>
2023-12-10 22:46:23 -0600

    Add: Migración v1 yv2 para tabla de paises (Creación y llenado con paises hispano hablantes)

--- src/main/resources/db/migration/V1__CREATE_TABLE_PAISES.sql
+++ src/main/resources/db/migration/V1__CREATE_TABLE_PAISES.sql
@@ -1,7 +1,6 @@
 CREATE TABLE paises (
     id bigint not null auto_increment,
-    codigo_pais varchar(3) not null unique,
-    nombre_pais varchar(25) not null unique,
-
+    codigo_pais varchar(5) not null unique,
+    nombre_pais varchar(75) not null unique,
     primary key(id)
 );
