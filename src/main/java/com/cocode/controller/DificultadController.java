6be66bc3eb9be35301a8d39e210008140b495c1c
Joshua Frías <metatron1495@gmail.com>
2023-12-13 02:16:42 -0600

    Add: Concluida lógica de petición para tabla dificultades

--- src/main/java/com/cocode/controller/DificultadController.java
+++ src/main/java/com/cocode/controller/DificultadController.java
@@ -1,5 +1,6 @@
 package com.cocode.controller;
 
+import com.cocode.model.dto.DificultadDto;
 import com.cocode.model.entity.Dificultad;
 import com.cocode.model.payload.ResponseMessage;
 import com.cocode.service.IDificultad;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,22 @@ public class DificultadController {
 
         return new ResponseEntity<>(ResponseMessage.builder().message("")
                 .object(getList).build(), HttpStatus.OK);
+    }
+
+    @GetMapping("dificultad/{id}")
+    public ResponseEntity<?> showById(@PathVariable Long id) {
+        Dificultad dificultad = dificultadService.findById(id);
+
+        if (dificultad == null) {
+            return new ResponseEntity<>(ResponseMessage.builder().message("El registro que estás buscando no ha podido ser encontrado")
+                    .object(null).build(), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(ResponseMessage.builder().message("").object(
+                DificultadDto.builder()
+                        .id(dificultad.getId())
+                        .nombre(dificultad.getNombre())
+                        .build()
+        ).build(), HttpStatus.OK);
     }
 }
